8c48ff696712da88455adbb5502dcb3b30d70144
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-08-31 00:31:20 +0300

    Add Create DTOs for customer and Employee

---
+++ src/main/java/com/carservice/dto/CreateCustomerDTO.java
@@ -0,0 +1,41 @@
+package com.carservice.dto;
+
+import com.carservice.data.entity.Appointment;
+import com.carservice.data.entity.Car;
+import com.carservice.data.entity.CarCenter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class CreateCustomerDTO {
+    @NotBlank(message = "The email cannot be empty!")
+    @Size(min = 6, max = 64, message = "The email must be at between {min} and {max} characters long!")
+    protected String email;
+
+    @NotBlank(message = "The first name cannot be empty!")
+    protected String firstName;
+
+    @NotBlank(message = "The last name cannot be empty!")
+    protected String lastName;
+
+    @NotBlank(message = "The phone number cannot be empty!")
+    @Pattern(regexp = "^\\+359\\d{9}$", message = "The phone number must be in the format +359xxxxxxxxx!")
+    protected String phoneNumber;
+
+    @NotBlank(message = "The password cannot be empty!")
+    @Size(min = 8, max = 64, message = "The password must contain at least 8 characters!")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "The password must contain at least 1 uppercase letter, 1 lowercase letter and 1 digit!")
+    protected String password;
+}
