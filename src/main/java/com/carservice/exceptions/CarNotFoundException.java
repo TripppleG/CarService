a57c51da10ac3eb9b5c0c013bcbf50a9ab975541
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-09-10 23:41:53 +0300

    Add Not found exceptions

--- src/main/java/com/carservice/exceptions/CarNotFoundException.java
+++ src/main/java/com/carservice/exceptions/CarNotFoundException.java
@@ -1,5 +1,9 @@
 package com.carservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class CarNotFoundException extends RuntimeException {
     public CarNotFoundException(String message) {
         super(message);
--- src/main/java/com/carservice/exceptions/ServiceJobNotFoundException.java
+++ src/main/java/com/carservice/exceptions/ServiceJobNotFoundException.java
@@ -1,5 +1,9 @@
 package com.carservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ServiceJobNotFoundException extends RuntimeException {
     public ServiceJobNotFoundException(String message) {
         super(message);
