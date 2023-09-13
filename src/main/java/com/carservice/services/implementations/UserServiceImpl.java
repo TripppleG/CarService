6bc1cd216db39d87ba8e14fca51fc133a22844c2
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-09-14 02:52:11 +0300

    Update the User Service according to the previous changes of the User class

--- src/main/java/com/carservice/services/implementations/UserServiceImpl.java
+++ src/main/java/com/carservice/services/implementations/UserServiceImpl.java
@@ -1,9 +1,6 @@
 package com.carservice.services.implementations;
 
-import com.carservice.data.entity.Employee;
 import com.carservice.data.entity.User;
-import com.carservice.data.repository.CustomerRepository;
-import com.carservice.data.repository.EmployeeRepository;
 import com.carservice.data.repository.UserRepository;
 import com.carservice.services.UserService;
 import lombok.AllArgsConstructor;
@@ -11,25 +8,17 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
-    private final CustomerRepository customerRepository;
-    private final EmployeeRepository employeeRepository;
+    private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User customer = customerRepository.findByEmail(email);
-        User employee = employeeRepository.findByEmail(email);
-
-        if (customer != null){
-            return customer;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
         }
-
-        if (employee != null){
-            return employee;
-        }
-        throw new UsernameNotFoundException("Username not found");
+        return user;
     }
 }
