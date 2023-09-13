62927f36bbac98bbe8b2fdd58dcccb8f471cfc28
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-09-14 02:47:18 +0300

    Add findByEmail method

--- src/main/java/com/carservice/data/repository/UserRepository.java
+++ src/main/java/com/carservice/data/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.carservice.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    User findByEmail(String username);
 }
